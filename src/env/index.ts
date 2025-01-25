f96800a9cf714df72ff1d6402e68d535a80de924
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-01-25 11:37:26 -0300

    feat: Add Gemini key on env.

---
+++ src/env/index.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  GEMINI_API_KEY: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  throw new Error(`Invalid environment variables: ${_env.error.errors}`)
+}
+
+export const env = _env.data
